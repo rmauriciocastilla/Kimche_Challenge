bb7750c55f2df80b998691b29b3a445bb507ab2d
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-09-05 12:55:24 -0500

    Feature(Connect Provider) - Connect Provider with APP.JS and display data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_COUNTRIES = gql`
+  query {
+    countries{
+      code
+      name
+      native
+      phone
+      continent{
+        code
+        name
+      }
+      currency
+      languages{
+        code
+        name
+        native
+      }
+      emoji
+      emojiU
+      states{
+        code
+        name
+      }
+    }
+  }
+`
+
 
 function App() {
+  const {data,error,loading} = useQuery(ALL_COUNTRIES);
+  if(error) return <span style='color: red'>{error}</span>
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {loading ? <p>loading</p> :(
+            <>
+            <p> GraphQL + React </p>
+            {data && data.countries.map(c=>c.name).join(', ')}
+            </>
+            )}
       </header>
     </div>
   );
